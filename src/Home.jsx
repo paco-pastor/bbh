b19ebe0bfafe8575474763bee81d3ee2d189aa83
Paco-Pastor <pacopas1512@gmail.com>
2024-02-03 22:47:29 +0100

    🎉 Initial commit

---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      {/* Affichez les résultats ou d'autres informations ici */}
+    </div>
+  );
+}
+
+export default Home;
